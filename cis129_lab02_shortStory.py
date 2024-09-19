cfdefc80131570ec1c5dd3644b8051a82a7354da
Juniper Vermillo <jvermillo@mail.pima.edu>
2024-09-19 01:33:47 -0700

    Add comments to short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,3 +1,8 @@
+# Juniper Vermillo
+# CIS 129
+# 9/19/2024
+# cis_lab02_shortStory.py - Prints a short story about how Newton's idea of gravity came to be
+
 print('Newton, feeling stumped, rested by a tree')
 print('An apple, dangling off it, fell as the breeze blows')
 print('He spots the apple, and an idea came to be')
